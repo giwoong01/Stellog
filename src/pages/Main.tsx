f9cef3f1b3d160666ccd3386d20b11f8b3db513c
giwoong <dev.choi0409@gmail.com>
2025-04-19 00:07:03 +0900

    feat(#8): 사용자 정보, 로그인/로그아웃 바 구현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,14 +3,38 @@ import useKakaoMap from "../hooks/useKakaoMap";
 import LocationDetailModal from "../components/modals/LocationDetailModal";
 import locations from "../data/locations.json";
 import styled from "styled-components";
+import MemberInfo from "../components/MemberInfo";
 
 const Main: React.FC = () => {
   const [selectedLocation, setSelectedLocation] = useState<any>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [memberInfo, setMemberInfo] = useState<{
+    name: string;
+    imageUrl: string;
+  } | null>(null);
 
   const handleMarkerClick = (location: any) => {
     setSelectedLocation(location);
     setIsModalOpen(true);
+  };
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    setMemberInfo({
+      name: "웅이",
+      imageUrl:
+        "https://storage.googleapis.com/image-gcs/project/3/33190dd8-ad66-4186-ba4e-e2763f371b5b",
+    });
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setMemberInfo({
+      name: "웅이",
+      imageUrl:
+        "https://storage.googleapis.com/image-gcs/project/3/33190dd8-ad66-4186-ba4e-e2763f371b5b",
+    });
   };
 
   useKakaoMap("map", locations, handleMarkerClick);
@@ -26,6 +50,13 @@ const Main: React.FC = () => {
           location={selectedLocation}
         />
       )}
+
+      <MemberInfo
+        isLoggedIn={isLoggedIn}
+        userInfo={memberInfo}
+        onLogin={handleLogin}
+        onLogout={handleLogout}
+      />
     </MainContainer>
   );
 };
